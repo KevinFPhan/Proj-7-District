8ae0a3d5624f793a0bbd5d108b0c574d1b85b4a2
Kevin Phan <90020140+KevinFPhan@users.noreply.github.com>
2022-01-27 22:51:08 -0800

    1st draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'soldier': "url('https://desque.sfo3.cdn.digitaloceanspaces.com/kevin/DistrictSecurityForce/DistrictSecurityForce_Wallpaper-1024x576.png')",
+      }
+    },
   },
   plugins: [],
 }
